319e7c9bfb71039f93d5517b2c12d924733b797a
Dario <Dario@aula2-PC13>
2015-05-22 13:53:05 +0200

    ADD Carpetas añadidas Formulario

--- src/es/curso/persistence/model/dao/jdbc/ClienteDaoJdbc.java
+++ src/es/curso/persistence/model/dao/jdbc/ClienteDaoJdbc.java
@@ -1,5 +1,18 @@
 package es.curso.persistence.model.dao.jdbc;
 
-public class ClienteDaoJdbc {
+import es.curso.model.entity.Cliente;
+import es.curso.persistence.model.dao.ClienteDao;
+
+public class ClienteDaoJdbc implements ClienteDao{
+
+	@Override
+	public void create(Cliente cliente) {
+		// van las instrucciones
+		//1.Conectar con la base de datos
+		//2.Preparar las sentencias-->SQL<-- para agregar
+		//3.Ejecutar la sentecia -->SQL<--
+		//4.Cerrar la conexion
+		
+	}
 
 }
